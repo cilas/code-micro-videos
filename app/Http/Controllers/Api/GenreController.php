be7f248cd4c6d58d05c47b35fae2f86b6d2d4cb4
cilas <cilas.acms@gmail.com>
2020-12-02 23:08:30 -0300

    test: add http tests for Genre Controller

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -16,7 +16,9 @@ class GenreController extends Controller
 
     public function store(GenreRequest $request)
     {
-        return Genre::create($request->validated());
+        $genre = Genre::create($request->validated());
+        $genre->refresh();
+        return $genre;
     }
 
     public function show(Genre $genre)
